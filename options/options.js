0b45dda368031849f6896eefb9843ede67b4d048
Crowdbotics <team@crowdbotics.com>
2023-05-13 09:35:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-firefly-40670",
+  url: "https://fancy-firefly-40670.botics.co",
+  api: "https://fancy-firefly-40670.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
